54e8f1ec83810978438036964a673f30d1d9fb66
NamKarin <91411888+NamKarin@users.noreply.github.com>
2021-10-04 02:46:29 +0700

    Update line.php

--- line.php
+++ line.php
@@ -14,7 +14,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => '#ใส่ ID HERE',
+        'to' => 'U8ed79a125acd83f1dcff3ad40b23c856',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
