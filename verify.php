b69746c0dc7367c44fc5ecac6fe3cde751edbe24
NamKarin <91411888+NamKarin@users.noreply.github.com>
2021-10-04 03:24:42 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = '#ใส่ access_token';
+$access_token = 'HfnSmBlVMunz39ggqzJa69ySgb3kP3kpZyZWdXQjYhqP1WLdStU6RKHtq2Jw+jqyaiHpY+G3mVo0C5tWGE/OIHcgY/8cNfaael+FJfoAyatrd8sv/dYN4hFsu5QPalOpd+VGv1fAYzBzjiipBhQrvgdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
